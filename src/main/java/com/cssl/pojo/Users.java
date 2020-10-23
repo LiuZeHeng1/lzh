a605ab1c06d225a545d6ff8efe3680b1172cb1d6
zeheng <2590757239@qq.com>
2020-10-23 14:55:59 +0800

    Revert "第一次提交码云33"
    This reverts commit 5b84e9e3

--- src/main/java/com/cssl/pojo/Users.java
+++ src/main/java/com/cssl/pojo/Users.java
@@ -15,28 +15,4 @@ public class Users {
     public void setAge(Integer age) {
         this.age = age;
     }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSex() {
-        return sex;
-    }
-
-    public void setSex(String sex) {
-        this.sex = sex;
-    }
 }
