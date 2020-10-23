a8d9b1d0a7f49cc5ba7237312f2a0cd44767b62c
zeheng <2590757239@qq.com>
2020-10-23 14:56:21 +0800

    Revert "第一次提交码云"
    This reverts commit e0018f4f

--- src/main/java/com/cssl/pojo/Users.java
+++ src/main/java/com/cssl/pojo/Users.java
@@ -3,16 +3,8 @@ package com.cssl.pojo;
 public class Users {
     private Integer id;
     private String name;
-    private String sex;
-    private Integer age;
-    //测试
 
 
-    public Integer getAge() {
-        return age;
-    }
 
-    public void setAge(Integer age) {
-        this.age = age;
-    }
+
 }
